8f28be8fd9e7c17d35b981a70bdcdf6b646976ba
anon <anon@anon.anon>
2022-11-16 10:38:10 +0100

    color pair enum

--- src/tui.cpp
+++ src/tui.cpp
@@ -14,7 +14,11 @@ static size_t state = 0;
 inline WINDOW* wmain;
 inline WINDOW* whelpbar;
 
-
+enum {
+	COLOR_PAIR_SELECTION = 1,
+	COLOR_PAIR_CURSOR,
+	COLOR_PAIR_HELP
+};
 
 bool tui_init(){
 	initscr();
@@ -25,8 +29,9 @@ bool tui_init(){
 	getmaxyx(stdscr, scrh, scrw);
 
 	start_color();
-	init_pair(1, COLOR_WHITE, COLOR_RED);	// Selection
-	init_pair(2, COLOR_BLACK, COLOR_WHITE);	// Help
+	init_pair(COLOR_PAIR_SELECTION, COLOR_WHITE, COLOR_RED);
+	init_pair(COLOR_PAIR_CURSOR, COLOR_WHITE, COLOR_BLUE);
+	init_pair(COLOR_PAIR_HELP, COLOR_BLACK, COLOR_WHITE);
 
 	wmain = newwin(scrh-1, scrw, 0, 0);
 	whelpbar = newwin(1, scrw, scrh-1, 0);
@@ -47,11 +52,11 @@ void tui_services(){
 	for(int i = 0; i < services.size(); i++){
 		char* buf = services[i]->pretty_render(scrw-2);
 		if(i == selection){
-			wattron(wmain, A_BOLD | COLOR_PAIR(1));
+			wattron(wmain, A_BOLD | COLOR_PAIR(COLOR_PAIR_SELECTION));
 		}
 		mvwaddstr(wmain, lineno+1, 1, buf);
 		if(i == selection){
-			wattroff(wmain, A_BOLD | COLOR_PAIR(1));
+			wattroff(wmain, A_BOLD | COLOR_PAIR(COLOR_PAIR_SELECTION));
 		}
 		++lineno;
 		delete buf;
@@ -66,9 +71,9 @@ void tui_help(){
 	werase(whelpbar);
 	for(int i = 0; HELP_MSG[state][i] != NULL; i++){
 		waddch(whelpbar, ' ');
-		wattron(whelpbar, COLOR_PAIR(2));
+		wattron(whelpbar, COLOR_PAIR(COLOR_PAIR_HELP));
 		waddstr(whelpbar, HELP_MSG[state][i]);
-		wattroff(whelpbar, COLOR_PAIR(2));
+		wattroff(whelpbar, COLOR_PAIR(COLOR_PAIR_HELP));
 	}
 }
 
