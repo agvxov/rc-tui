8001cc6b2f54b63c4342405c099ed6f129fadf12
anon <anon@anon.anon>
2022-11-29 23:36:04 +0100

    execve werks - debug mode on

--- src/Service.hpp
+++ src/Service.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #define SERVICE_MAX_STATUS_LEN		10
 #define SERVICE_MAX_RUNLEVEL_LEN	23
@@ -18,17 +19,22 @@ struct Service {
 	std::string name;
 	std::string runlevel;
 	std::string status;
-	bool locked = true;
+	bool locked = false;
 
 	void change_status(const int &st){
+		if(this->locked){ return; }
 		char *const to_cmd = strdup(((char**)cmd[(bool)strcmp(this->status.c_str(), "[started]")])[st]);
 		char *const name = strdup(this->name.substr(1, this->name.size()-2).c_str());
-		char* argv[] = {"rc-status", name, to_cmd, NULL};
+		char* argv[] = {"rc-service", name, to_cmd, NULL};
 
 		const int cp = fork();
 		if(cp == -1){ return; }
 		this->locked = true;
 		if(cp == 0){
+			fclose(stdout);
+			fclose(stderr);
+			int iw = open("debug/debug.log", O_WRONLY | O_CREAT, 0644);
+			int iv = open("debug/debug.log", O_WRONLY | O_CREAT, 0644);
 			execvpe(argv[0], argv, NULL);
 		}
 	}
